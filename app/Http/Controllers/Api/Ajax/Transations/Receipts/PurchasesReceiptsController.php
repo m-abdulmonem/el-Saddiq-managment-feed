a382f1ae2a216fdddefc14a78f0156364904b94d
mabdulmonem <mabdelmonem@serv5.com>
2025-01-11 22:17:40 +0200

    fix

--- app/Http/Controllers/Api/Ajax/Transations/Receipts/PurchasesReceiptsController.php
+++ app/Http/Controllers/Api/Ajax/Transations/Receipts/PurchasesReceiptsController.php
@@ -24,7 +24,7 @@ class PurchasesReceiptsController extends Controller
     public function __invoke(Request $request)
     {
         if (request()->ajax())
-            return datatables()->of(CatchPurchase::latest())
+            return datatables()->of(CatchPurchase::latest()->get())
                 ->addIndexColumn()
                 ->addColumn("number",function ($data){
                     return $data->invoices->code;
